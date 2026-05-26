bee0f4b049e6cc837c85bb2123f609e6b8ef9d7d
flifd <ciaranjbrennan@proton.me>
2026-05-26 20:21:18 +0100

    Created home screen

--- src/main/routes/home.ts
+++ src/main/routes/home.ts
@@ -5,9 +5,18 @@ export default function (app: Application): void {
   app.get('/', async (req, res) => {
     try {
       // An example of connecting to the backend (a starting point)
-      const response = await axios.get('http://localhost:4000/get-example-case');
+      const response = await axios.get('http://localhost:4000/tasks');
       console.log(response.data);
-      res.render('home', { "example": response.data });
+      res.render('home', { "tasks": response.data, "taskSize": response.data.length });
+    } catch (error) {
+      console.error('Error making request:', error);
+      res.render('home', {});
+    }
+  });
+
+  app.get('/tasks/new', async (req, res) => {
+    try {
+      res.render('createtaskform');
     } catch (error) {
       console.error('Error making request:', error);
       res.render('home', {});
